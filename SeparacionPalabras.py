b9bc7bb495c817cd354f421169149f9620759270
Daniel <111716153+Rhema1517@users.noreply.github.com>
2024-02-02 00:42:36 -0500

    Update SeparacionPalabras.py

--- SeparacionPalabras.py
+++ SeparacionPalabras.py
@@ -3,7 +3,7 @@ def to_underscore(string):
     lista = []
     for n,l in enumerate(string):
         if l == l.upper()and not n == 0 and not l.isdigit():#isdigites si es un numero devuelve true
-            lista.append('_')
+            lista.append(' ')
         lista.append(l.lower())
     return ''.join(lista)
 to_underscore(1)
